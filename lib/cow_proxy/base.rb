59ecf8491326f2a289198ed184fcd567054c287b
Sergio Cambra <sergio@programatica.es>
2017-03-21 09:32:41 +0100

    refactor __wrapped_value__, remove unneeded arg

--- lib/cow_proxy/base.rb
+++ lib/cow_proxy/base.rb
@@ -28,14 +28,13 @@ module CowProxy
       # @return [Proc] Block with proxy implementation.
       def wrapping_block(method, cow_enabled)
         lambda do |*args, &block|
-          target = __getobj__
           inst_var = "@#{method}" if method.to_s =~ /^\w+$/
           return _instance_variable_get(inst_var) if inst_var && _instance_variable_defined?(inst_var)
           if method.to_s =~ /^(\w+)=$/ && _instance_variable_defined?("@#{$1}")
             CowProxy.debug "remove #{$1}"
             _remove_instance_variable "@#{$1}"
           end
-          __wrapped_value__(inst_var, cow_enabled, target, method, *args, &block)
+          __wrapped_value__(inst_var, cow_enabled, method, *args, &block)
         end
       end
     end
@@ -91,15 +90,17 @@ module CowProxy
       end
     end
 
-    def __wrapped_value__(inst_var, cow_enabled, target, method, *args, &block)
-      CowProxy.debug "run on #{target.class.name} (#{target.object_id}) #{method} #{args.inspect unless args.empty?}"
-      value = target.__send__(method, *args, &block)
+    def __wrapped_value__(inst_var, cow_enabled, method, *args, &block)
+      CowProxy.debug "run on #{__getobj__.class.name} (#{__getobj__.object_id}) #{method} #{args.inspect unless args.empty?}"
+      value = __getobj__.__send__(method, *args, &block)
       wrap_value = __wrap__(value, inst_var) if inst_var && args.empty? && block.nil?
       wrap_value || value
     rescue => e
       raise unless cow_enabled && e.message =~ /^can't modify frozen/
       CowProxy.debug "copy on write to run #{method} #{args.inspect unless args.empty?} (#{e.message})"
-      __wrapped_value__(inst_var, false, __copy_on_write__, method, *args, &block)
+      __copy_on_write__
+      CowProxy.debug "new target #{__getobj__.class.name} (#{__getobj__.object_id})"
+      __wrapped_value__(inst_var, false, method, *args, &block)
     end
 
     alias :_instance_variable_get :instance_variable_get
