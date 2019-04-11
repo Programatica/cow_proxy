be62080b5bd0524d64db4f589fc306eb50d3c69d
Sergio Cambra <sergio@programatica.es>
2019-04-11 09:30:18 +0200

    bump to 0.3.3

--- lib/cow_proxy/version.rb
+++ lib/cow_proxy/version.rb
@@ -2,8 +2,8 @@ module CowProxy
   module Version
     MAJOR = 0
     MINOR = 3
-    PATCH = 2
+    PATCH = 3
 
-    STRING = [MAJOR, MINOR, PATCH].compact.join('.')
+    STRING = [MAJOR, MINOR, PATCH].compact.join('.').freeze
   end
 end
